8a86839d1c4975d9b8083ca0e4b0e7009fa69d3d
macosx <macosx@macbook-pro-yokaisx.home>
2022-06-30 14:47:31 +0200

    task1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const element = document.querySelector(`.comments__item.comments__item--newest` )
+if(element !== null) {
+    const elementWithAtrr = element.querySelectorAll('[data-info]')
+    console.log(elementWithAtrr.length)
+}
+
+

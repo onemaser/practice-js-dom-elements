693078664f8118c62602c1722d8376593eed1b27
macosx <macosx@macbook-pro-yokaisx.home>
2022-07-04 17:06:37 +0200

    done DOM git push clear

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,19 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const button = document.querySelector('.parent-for-button')
+const newButton = document.createElement('button')
+newButton.innerText = buttonSettings.text
+
+button.appendChild(newButton)
+
+for(const key in buttonSettings.attr) {
+    const value = buttonSettings.attr[key]
+    newButton[key] = value
+}
+
+for(const key in buttonSettings.css) {
+    const value = buttonSettings.css[key]
+    newButton.style[key] = value
+}
--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,34 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const newButton = document.createElement('button')
+newButton.innerText = `usuń z koszyka`
+curr.parentElement.appendChild(newButton)
+
+const children =Array.from(curr.parentElement.children)
+console.log(children)
+
+children.forEach(function(item) {
+    if(item !== curr) {
+        item.classList.add('siblings')
+    }
+}) 
+const firstArticle = curr.parentElement
+
+const siblingElement = firstArticle.nextElementSibling
+console.log(siblingElement)
+
+siblingElement.title = 'nextElementSibling'
+
+const lastArticle = firstArticle.nextElementSibling.nextElementSibling
+const btn = lastArticle.children[lastArticle.children.length -1];   
+console.log(btn) 
+
+const pElement = document.createElement('p')
+pElement.innerText = 'jakis tekst'
+
+lastArticle.insertBefore(pElement, btn)
+
+const newArticle = firstArticle.cloneNode(true)
+firstArticle.parentElement.insertBefore(newArticle, firstArticle)
